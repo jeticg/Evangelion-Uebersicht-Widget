cf42c3e708018d27f05a2ecce6c4b0e8f4086d08
Jetic Gu <jetic@me.com>
2017-07-06 15:50:26 -0700

    Fixing an issue that occurs when iTunes freezes.

--- Eva.widget/iTunes.sh
+++ Eva.widget/iTunes.sh
@@ -1,7 +1,17 @@
-ORZ=$(osascript 'Eva.widget/iTunes.scpt')
-if [[ $ORZ != *¶*¶*¶*¶* ]];
-then
-    echo "¶ ¶ ¶ 0 ¶ 0"
-else
-    echo $ORZ
-fi
+run(){
+    if [ ! -f Eva.widget/iTunes.working ]; then
+        touch Eva.widget/iTunes.ipworking
+        ORZ=$(osascript 'Eva.widget/iTunes.scpt')
+        if [[ $ORZ != *¶*¶*¶*¶* ]];
+        then
+            echo "¶ ¶ ¶ 0 ¶ 0" > Eva.widget/iTunes.output
+        else
+            echo "${ORZ}" > Eva.widget/iTunes.output
+        fi
+        rm Eva.widget/iTunes.ipworking
+    fi
+}
+
+run &>/dev/null &disown
+
+cat Eva.widget/iTunes.output
