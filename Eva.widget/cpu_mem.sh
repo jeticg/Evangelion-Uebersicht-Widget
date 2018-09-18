2d9bfd1bdef0d0ba06bbf1635b8145136dc5b550
Jetic Gū <jeticg@users.noreply.github.com>
2018-09-18 16:46:20 -0700

    Update cpu_mem.sh

--- Eva.widget/cpu_mem.sh
+++ Eva.widget/cpu_mem.sh
@@ -1,4 +1,4 @@
-ps aux  | awk 'BEGIN { sum = 0 }  { sum += $3 }; END { print sum }'
+ps -A -o %cpu | awk '{s+=$1} END {print s}'
 sysctl -a kern.memorystatus_vm_pressure_level | awk '{ print $2 }'
 #echo "1200\n100"
 sysctl hw.ncpu | awk '{print $2}'
